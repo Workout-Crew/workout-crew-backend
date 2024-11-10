817461a576dd67667c02eb8426541d93c3f9615c
sktmdgus1212 <sktmdgus1212@naver.com>
2024-11-11 01:22:48 +0900

    feat: allow localhost:3000 with cors

---
+++ src/main/kotlin/com/comtongsu/exercise/global/config/WebConfig.kt
@@ -0,0 +1,19 @@
+package com.comtongsu.exercise.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+            )
+            .allowedMethods("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowCredentials(true)
+            .allowedHeaders("*")
+    }
+}
